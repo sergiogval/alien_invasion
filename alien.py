1a7285daa0e235c03a498ffa1237ffa7ac2dd9cc
sergiogval <67393791+sergiogval@users.noreply.github.com>
2021-10-31 23:53:45 -0600

    Final

--- alien.py
+++ alien.py
@@ -10,7 +10,7 @@ class Alien(Sprite):
 		self.ai_settings = ai_settings
 
 		# Load the alien image and set its rect attribute.
-		self.image = pygame.image.load('images/alien.png')
+		self.image = pygame.image.load('images/alien.bmp')
 		self.rect = self.image.get_rect()
 
 		# Start each new alien near the top left of the screen.
--- ship.py
+++ ship.py
@@ -10,7 +10,7 @@ class Ship(Sprite):
 		self.ai_settings = ai_settings
 
 		# Load the ship image and get its rect.
-		self.image = pygame.image.load('images/ship.png')
+		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
 
